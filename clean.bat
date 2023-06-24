e2a019ad516176fc2fe75ef1eaeca925222003ae
multi-vitamin <smoil2000@paran.com>
2023-06-24 22:48:29 +0900

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,9 @@
+set PATH=C:\devkitPro\msys2\usr\bin;C:\devkitPro\devkitARM\bin;%PATH%
+set DEVKITARM=/c/devkitPro/devkitARM
+set DEVKITPRO=/c/devkitPro
+set LIBGBA=/c/devkitPro/libgba
+
+make  clean
+
+pause
+build.bat
